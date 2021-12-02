cfcbf28d8ffa9ede7d7992da82f0266cfe368e2c
Konstantin Gareev <gareevk@hotmail.com>
2021-12-02 23:58:36 +0300

    fix: fix after review

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let popup = document.querySelector('.popup');
 let editProfile = document.querySelector('.profile__edit');
 let closePopup = document.querySelector('.popup__close-button');
-let nameInput = popup.querySelector('.popup__name-input');
-let proInput = popup.querySelector('.popup__bio-input');
+let nameInput = popup.querySelector('#name-input');
+let proInput = popup.querySelector('#bio-input');
 let profileName = document.querySelector('.profile__name');
 let profileBio = document.querySelector('.profile__description');
 let profileSubmit = popup.querySelector('.popup__container');
@@ -19,9 +19,11 @@ function submitProfileEdit(evt) {
     evt.preventDefault();
     if (nameInput.value !== '') {
         profileName.textContent = nameInput.value;
+        nameInput.setAttribute("value", nameInput.value);
     }
     if (proInput.value !== '') {
         profileBio.textContent = proInput.value;
+        proInput.setAttribute("value", proInput.value);
     }
     popupClose();
 }
