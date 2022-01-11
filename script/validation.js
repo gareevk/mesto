85d343c89b13ce070b810a979767cf2ab0a9e902
Konstantin Gareev <gareevk@hotmail.com>
2022-01-11 10:27:45 +0300

    feat: add forms validation, popup closing on focus lost and Esc

--- script/validation.js
+++ script/validation.js
@@ -1,27 +1,45 @@
+/*Button validation*/
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  }; 
+  
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add('popup__save-button_disabled');
+      buttonElement.setAttribute('disabled', true);
+    } else {
+      buttonElement.classList.remove('popup__save-button_disabled');
+      buttonElement.removeAttribute('disabled');
+    }
+}
+
 /*profile form validation*/
 const showInputError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
     inputElement.classList.add('popup__input_type_error');
     errorElement.textContent = errorMessage;
     errorElement.classList.add('popup__input-error_active');
-  }
+}
   
-  const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
     inputElement.classList.remove('popup__input_type_error');
     errorElement.classList.remove('popup__input-error_active');
     errorElement.textContent = '';
-  };
+};
   
-  const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement) => {
     if (!inputElement.validity.valid) {
       showInputError(formElement, inputElement, inputElement.validationMessage);
     } else {
       hideInputError(formElement, inputElement);
     }
-  };
-  
-  const setEventListeners = (formElement) => {
+};
+
+
+const setEventListeners = (formElement) => {
     const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
     const buttonElement = formElement.querySelector('.popup__save-button');
     toggleButtonState(inputList, buttonElement);
@@ -31,9 +49,9 @@ const showInputError = (formElement, inputElement, errorMessage) => {
         toggleButtonState(inputList, buttonElement);
       });
     });
-  };
+};
   
-  function enableValidation () {
+function enableValidation () {
     const formList = Array.from(document.querySelectorAll('.popup__container'));
     
     formList.forEach((formElement) => {
@@ -43,10 +61,13 @@ const showInputError = (formElement, inputElement, errorMessage) => {
   
       setEventListeners(formElement);
     });
-  }
+}
   
-  enableValidation();
+enableValidation();
+
+
   
+  /*
   const clearValidation = (popup) => {
     const errorMessage = Array.from(popup.querySelectorAll('.popup__input-error'));
     errorMessage.forEach((error) => {
@@ -59,3 +80,4 @@ const showInputError = (formElement, inputElement, errorMessage) => {
     const buttonDisabled = popup.querySelector('.popup__save-button');
     buttonDisabled.classList.remove('popup__save-button_disabled');
   }
+*/
