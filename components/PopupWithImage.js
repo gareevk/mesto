4ef88740a701966d5aa62da23635d5bccf49f02a
Konstantin Gareev <gareevk@hotmail.com>
2022-02-07 23:15:34 +0300

    feat: added UserInfo class, added implementation of UserInfo class to set profile info

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -12,8 +12,3 @@ export default class PopupWithImage extends Popup {
         super.open();
     }
 }
-
-/*
-Создайте класс PopupWithImage
-Создайте класс PopupWithImage, который наследует от Popup. Этот класс должен перезаписывать родительский метод open. В методе open класса PopupWithImage нужно вставлять в попап картинку с src изображения и подписью к картинке.
-*/
